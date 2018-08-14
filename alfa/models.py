a2103213e57da6be1fa5eeb747a82a6d9b832e78
hectopk <hectopk@gmail.com>
2018-08-14 17:15:53 +0300

    #4 Страницы создания/редактирования проекта

--- alfa/models.py
+++ alfa/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import User
 
 class Project(models.Model):
 	name = models.CharField(max_length=100, default='')
+	alias = models.CharField(max_length=100, default='')
 	user = models.ForeignKey(User ,on_delete=models.CASCADE, related_name='projects', null=True, default=None)
--- alfa/views.py
+++ alfa/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from alfa.models import *
+from alfa.forms import *
+import re
 
 def has_premission():
 	def has(f):
@@ -29,7 +31,31 @@ def config_page(request):
 @has_premission()
 def new_project_page(request):
 	context = {}
-	return render(request, 'new_project_page.html', context)
+	context['header'] = 'New project'
+	if request.method == 'POST':
+		form = ProjectForm(request.POST)
+		if form.is_valid():
+			project = Project()
+			project.user = request.user
+			project.name = form.cleaned_data['name']
+			project.alias = form.cleaned_data['alias']
+			project.alias = re.sub('[^a-zA-Z0-9]', '', project.alias)
+			if project.alias == form.cleaned_data['alias']:
+				project.save()
+				return HttpResponseRedirect(reverse('dashboard_url'))
+			else:
+				context['name'] = form.cleaned_data['name']
+				context['alias'] = form.cleaned_data['alias']
+				context['error'] = True
+				context['error_message'] = 'Bad alias (only digits and characters available).'
+				return render(request, 'new_project_page.html', context)
+		else:
+			context['error'] = True
+			context['error_message'] = 'Bad form.'
+			return render(request, 'new_project_page.html', context)
+	else:
+		context['form'] = ProjectForm()
+		return render(request, 'new_project_page.html', context)
 
 @has_premission()
 def delete_project_page(request, id):
@@ -44,4 +70,33 @@ def delete_project_page(request, id):
 @has_premission()
 def edit_project_page(request, id):
 	context = {}
+	context['header'] = 'Edit project'
+	try:
+		project = Project.objects.get(id=id, user=request.user)
+	except Project.DoesNotExist:
+		return HttpResponseRedirect(reverse('dashboard_url'))
+	if request.method == 'POST':
+		form = ProjectForm(request.POST)
+		if form.is_valid():
+			project.name = form.cleaned_data['name']
+			project.alias = form.cleaned_data['alias']
+			project.alias = re.sub('[^a-zA-Z0-9]', '', project.alias)
+			if project.alias == form.cleaned_data['alias']:
+				project.save()
+				return HttpResponseRedirect(reverse('dashboard_url'))
+			else:
+				context['name'] = form.cleaned_data['name']
+				context['alias'] = form.cleaned_data['alias']
+				context['error'] = True
+				context['error_message'] = 'Bad alias (only digits and characters available).'
+				return render(request, 'new_project_page.html', context)
+		else:
+			context['error'] = True
+			context['error_message'] = 'Bad form.'
+			return render(request, 'new_project_page.html', context)
+	else:
+		context['form'] = ProjectForm()
+		context['name'] = project.name
+		context['alias'] = project.alias
+		return render(request, 'new_project_page.html', context)
 	return render(request, 'edit_project_page.html', context)
